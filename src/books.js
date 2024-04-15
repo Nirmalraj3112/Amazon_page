63360f3123cd969ff644237d815d2513ca8d1360
Nirmalraj3112 <nirmalraj.dharanikumar@gmail.com>
2024-04-15 10:44:44 +0530

    first commit

---
+++ src/books.js
@@ -0,0 +1,37 @@
+import img1 from './images/book-1.jpg';
+import img2 from './images/book-2.jpg';
+import img3 from './images/book-3.jpg';
+import img4 from './images/book-4.jpg';
+
+export const books = [
+  {
+    author: 'Ashley Elston',
+    title: `First Lie Wins: Reese's Book Club Pick (A Novel)`,
+    img: img1,
+    id: 1,
+  },
+  {
+    author: 'James Clear',
+    title: `Atomic Habits: An Easy & Proven Way to Build Good Habits & Break Bad Ones`,
+    img: img2,
+    id: 2,
+  },
+  {
+    author: 'Rebecca Yarros',
+    title: `Fourth Wing (The Empyrean, 1)`,
+    img: img3,
+    id: 3,
+  },
+  {
+    author: ' Rebecca Yarros',
+    title: `Iron Flame (The Empyrean, 2)`,
+    img: img4,
+    id: 4,
+  },
+  {
+    author: 'Ashley Elston',
+    title: `First Lie Wins: Reese's Book Club Pick (A Novel)`,
+    img: img1,
+    id: 1,
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+//importing the data books from the books.js
+import { books } from './books';
+import Book from './Book';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const BookList = () => {
+  return (
+    <>
+      <h1>amazon best sellers</h1>
+      <section className="booklist">
+        {books.map((book, index) => {
+          return <Book {...book} key={book.id} number={index} />;
+        })}
+      </section>
+    </>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<BookList />);
